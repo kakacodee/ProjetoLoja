40f72feac8e8990732064e53d8a4178b561e7f20
Kauã santos <131719356+kakacodee@users.noreply.github.com>
2025-08-20 08:33:10 -0300

    Update EcomLoja.sql

--- EcomLoja.sql
+++ EcomLoja.sql
@@ -1,2 +1,37 @@
 create database EcomLoja;
 use EcomLoja;
+
+create table produto(
+Id int primary key auto_increment,
+Nome varchar(50),
+Descricao varchar(100),
+Preco decimal(10,2),
+ImageUrl varchar(255),
+Estoque int
+
+);
+
+create table pedido(
+Id int primary key auto_increment ,
+DataPedido datetime,
+Total decimal(10,2),
+Status varchar(50),
+Endereco varchar(100),
+FormaPagamento varchar(100),
+Frete decimal (10,2)
+);
+
+
+create table itemPedido(
+Id int primary key auto_increment ,
+PedidoId int,
+ProdutoId int,
+Quantidade int,
+PrecoUnitario decimal(10,2)
+);
+
+-- CONSULTANDO AS TABELAS DO BANCO
+
+select * from produtos;
+select * from pedido;
+select * from itemPedido;
