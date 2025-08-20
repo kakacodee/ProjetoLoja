6296953476bec5f18d1a1087a51acadc01a5325f
kakacodee <kakas13.souza@gmail.com>
2025-08-20 08:25:47 -0300

    add controllers

---
+++ ProjetoLoja/Controllers/PedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoLoja.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
